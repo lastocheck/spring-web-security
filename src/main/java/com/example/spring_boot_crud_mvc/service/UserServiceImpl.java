7624656640863525db116618cc2009d8a57f6854
lastocheck <lastocheck@gmail.com>
2024-07-17 17:57:56 +0300

    Add repository layer, remove dao

--- src/main/java/com/example/spring_boot_crud_mvc/service/UserServiceImpl.java
+++ src/main/java/com/example/spring_boot_crud_mvc/service/UserServiceImpl.java
@@ -1,49 +1,43 @@
 package com.example.spring_boot_crud_mvc.service;
 
-import com.example.spring_boot_crud_mvc.dao.UserDao;
 import com.example.spring_boot_crud_mvc.model.User;
-import org.springframework.stereotype.Component;
+import com.example.spring_boot_crud_mvc.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
-@Component
+@Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
-
-    private final UserDao userDao;
-
-    public UserServiceImpl(UserDao userDao) {
-        this.userDao = userDao;
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public void save(User user) {
-        userDao.save(user);
+        userRepository.save(user);
     }
 
     @Override
     public void delete(int id) {
-
-        try {
-            userDao.delete(id);
-        } catch (NoSuchElementException e) {
-            throw new RuntimeException("can't find user for removal");
-        }
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("couldn't find user with id " + id));
+        userRepository.delete(user);
     }
 
     @Override
     public void update(User newUser) {
-        userDao.update(newUser);
+        userRepository.save(newUser);
     }
 
     @Override
     public User findById(int id) {
-        return userDao.findById(id).orElse(null);
+        return userRepository.findById(id).orElse(null);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return userDao.findAll();
+        return userRepository.findAll();
     }
 }
