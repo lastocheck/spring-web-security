e21376321069951cff0ae690402efd2526a4d651
lastocheck <lastocheck@gmail.com>
2024-07-19 18:00:51 +0300

    add role column to a users list on admin page

--- src/main/java/com/example/spring_boot_crud_mvc/model/Role.java
+++ src/main/java/com/example/spring_boot_crud_mvc/model/Role.java
@@ -35,4 +35,9 @@ public class Role implements GrantedAuthority {
     public String getAuthority() {
         return name;
     }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
